bbcd3eafda7aa6b0683263706b2f62b0f954a558
Martsynyuk <terr83@gmail.com>
2016-03-14 10:29:08 +0200

    1

--- basic/views/Users/index.php
+++ basic/views/Users/index.php
@@ -1,6 +1,9 @@
 <?php
 use yii\helpers\Html;
+use yii\helpers\Url;
 ?>
 
 <?= Html::encode($text); ?></br>
-<?= $text; ?>
+<?= $text; ?></br>
+<a href="<?php echo Url::to(['/site/say']); ?>">Site/say</a>
+
