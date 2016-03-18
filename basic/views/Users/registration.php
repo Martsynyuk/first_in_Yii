0c0a2a68dca73aa8b31831d26ead64d82566d25b
Martsynyuk <terr83@gmail.com>
2016-03-18 16:58:00 +0200

    1

---
+++ basic/views/Users/registration.php
@@ -0,0 +1,16 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+?>
+<div class="content_autorization">
+	<?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'username') ?>
+    <?= $form->field($model, 'password')->passwordInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+    </div>
+
+	<?php ActiveForm::end(); ?>
+</div>
