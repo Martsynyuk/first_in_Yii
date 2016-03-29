66d9b64832dee6492a6b021faf40b7f8c8ba5a41
Martsynyuk <terr83@gmail.com>
2016-03-29 14:11:32 +0300

    1

--- basic/models/Information.php
+++ basic/models/Information.php
@@ -15,7 +15,7 @@ class Information extends ActiveRecord
 				[['FirstName', 'LastName', 'Email'], 'required'],
 				['Email', 'email'],
 				[['Zip'], 'integer'],
-				['Home', 'match', 'pattern' => '/[0-9]/']
+				['Home', 'match', 'pattern' => '/[0-9-]/'] //http://www.yiiframework.com/doc-2.0/guide-tutorial-core-validators.html#match
 		];
 	}
 }
