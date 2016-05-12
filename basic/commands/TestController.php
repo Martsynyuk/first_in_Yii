528289ace742d1d1cb7c8cca69f322f4f7249a12
Martsynyuk <terr83@gmail.com>
2016-05-12 17:10:30 +0300

    test console comand

--- basic/commands/TestController.php
+++ basic/commands/TestController.php
@@ -2,9 +2,8 @@
 
 namespace app\commands;
 
-use Yii;
 use yii\console\Controller;
-use app\models\User;
+use app\models\Information;
 
 /**
  * This command create sql query to DB.
@@ -17,11 +16,28 @@ class TestController extends Controller
 	 */
 	public function actionIndex()
 	{
-		$test = new User();
-		$test->login = 'merelin';
-		$test->password = Yii::$app->getSecurity()->generatePasswordHash('123456');
+		$test = Information::find()->select(['id', 'Home', 'Work', 'Cell', 'Telephone'])->all();
 		
-		$test->insert();
+		foreach ($test as $val)
+		{
+			if($val['Home'] == $val['Telephone'])
+			{
+				$this->telephoneUpdate($val['id'], 'Home');
+			}
+			elseif($val['Work'] == $val['Telephone'])
+			{
+				$this->telephoneUpdate($val['id'], 'Work');
+			}
+			elseif($val['Cell'] == $val['Telephone'])
+			{
+				$this->telephoneUpdate($val['id'], 'Cell');
+			}
+		}
 	}
-
+	
+	public function telephoneUpdate($id, $telephone)
+	{
+		$update = new Information();
+		$update->updateAll(['Telephone' => $telephone], ['id' => $id]);
+	}
 }
